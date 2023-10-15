d5f9c18e24ee79f03ee72be46043eddeeaad25e7
YugoOtani <107762166+YugoOtani@users.noreply.github.com>
2023-10-15 17:37:52 +0900

    implement qstr.rs

--- src/qstr.rs
+++ src/qstr.rs
@@ -1,1 +1,77 @@
-pub type QstrIndex = usize;
+// this is based on micropython qstr implementation
+// https://github.com/micropython/micropython/blob/master/py/qstr.c
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct QstrIndex(usize);
+const POOLSIZE_MIN: usize = 1;
+
+#[derive(Debug)]
+pub struct QstrPool {
+    parent: Option<Box<QstrPool>>,
+    qstrs: Vec<String>,
+    total_prev_len: usize,
+}
+impl QstrPool {
+    pub fn empty() -> Self {
+        Self {
+            parent: None,
+            qstrs: Vec::with_capacity(POOLSIZE_MIN),
+            total_prev_len: 0,
+        }
+    }
+    fn _get(&self, n: usize) -> Option<&str> {
+        self.qstrs.get(n).map(|x| x.as_str())
+    }
+    pub fn get(&self, n: usize) -> Option<&str> {
+        let mut pool = self;
+        while n < pool.total_prev_len {
+            pool = pool.parent.as_ref().unwrap()
+        }
+        pool._get(n - pool.total_prev_len)
+    }
+    pub fn find(&self, s: &str) -> Option<QstrIndex> {
+        for (i, s2) in self.qstrs.iter().enumerate() {
+            if s2 == s {
+                return Some(QstrIndex(i + self.total_prev_len));
+            }
+        }
+        if let Some(ref par) = self.parent {
+            par.find(s)
+        } else {
+            None
+        }
+    }
+    pub fn insert(mut self, s: &str) -> Self {
+        match self.find(s) {
+            Some(_) => self,
+            None => {
+                let current = self.qstrs.capacity();
+                if current == self.qstrs.len() {
+                    let total_prev_len = current + self.total_prev_len;
+                    let mut qstrs = Vec::with_capacity(current * 2);
+                    qstrs.push(s.to_string());
+                    Self {
+                        parent: Some(Box::new(self)),
+                        qstrs,
+                        total_prev_len,
+                    }
+                } else {
+                    self.qstrs.push(s.to_string());
+                    self
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn qstrpool_test() {
+    let mut pool = QstrPool::empty();
+    for s in ["a", "b", "c", "d", "a", "c", "e", "f", "a"] {
+        pool = pool.insert(s)
+    }
+    for (i, s) in ["a", "b", "c", "d", "e", "f"].iter().enumerate() {
+        println!("{}", s);
+        assert_eq!(Some(QstrIndex(i)), pool.find(s));
+        assert_eq!(Some(*s), pool.get(i));
+    }
+}
--- src/token.rs
+++ src/token.rs
@@ -1,1 +1,24 @@
-use crate::emtypes::Num;
+use crate::{emtypes::Num, qstr::QstrIndex};
+
+pub enum Token {
+    KwInt,
+    Num(Num),
+    KwBool,
+    Bool,
+    KwNode,
+    KwFunc,
+    KwData,
+    KwInit,
+    Dot,
+    At,
+    RParen, // ()
+    LParen,
+    RBrace, // {}
+    LBrace,
+    RBracket, // []
+    LBracket,
+    Plus,
+    Minus,
+    Astarisk,
+    Id(QstrIndex),
+}
